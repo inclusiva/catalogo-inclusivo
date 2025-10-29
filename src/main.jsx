485eb6894d02daf85f6500db36a23e97c4227739
Amanda Jauregui <amandajauregui.dev@gmail.com>
2025-10-29 13:16:55 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import './css/global.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h1>Olá, mundo!</h1>
+  </StrictMode>,
+)
