2104561f5a07560208802f4900344a0a4ba3919d
Pedro Wantuir <pedrowantuirpaixao@gmail.com>
2025-11-06 16:27:09 -0300

    Layout finalizado e encorporado nas rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,8 +6,10 @@ import NotFound from "./pages/NotFound";
 
 export default function AppRoutes() {
     <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/movie/:id" element={ <MovieDetails /> } />
-        <Route path="*" element={ <NotFound/> }/>
+        <Route path="/" element={ <Layout /> }>
+            <Route index element={ <Home /> } />
+            <Route path="/movie/:id" element={ <MovieDetails /> } />
+            <Route path="*" element={ <NotFound/> }/>
+        </Route>
     </Routes>
 }
