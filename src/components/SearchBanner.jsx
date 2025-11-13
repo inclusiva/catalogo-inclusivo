b6b55f4f83eca7660748f01cc531bdeed85598b1
Edu Silva <edusilva18@gmail.com>
2025-11-13 17:19:10 -0300

    Co-authored-by: Aline Mariah <aalinemariah@users.noreply.github.com>

--- src/components/SearchBanner.jsx
+++ src/components/SearchBanner.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-const SearchBanner = ({ value, onChange, onclick }) => {
+const SearchBanner = ({ value, onChange, onClick }) => {
     return (
         <section className= "banner">
             <h1 className="banner-titulo">Bem-vindo (a)</h1>
             <p className="banner-subtitulo">Milhões de filmes para descobrir.Explore já.</p>
             <div className="area-busca">
-                <input type=""
+                <input type="text"
                 placeholder="digite o nome do filme"
                 value={value}
                 onChange={onChange}
                 className="busca-input"
                 />
-                <button onclick={onclick} className="busca-botao">Buscar</button>
+                <button onClick={onClick} className="busca-botao">Buscar</button>
                 
                 </div>
 
