92613aba233d5013945a5468175d441d9fa820c3
Edu Silva <edusilva18@gmail.com>
2025-11-13 17:25:20 -0300

    Update layout.jsx

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,8 +1,12 @@
-export default function Layout({children}) {
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+export default function Layout() {
     return (
         <>
             <Header />
-            {children}
+            <Outlet/>
             <Footer />
         </>
     )
