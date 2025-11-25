04ae990bb3c93e13c24d31da0a5a401d62289044
Wanti <pedrowantuirpaixao@gmail.com>
2025-11-25 16:57:14 -0300

    Página notFound implementada. CSS pendente

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,17 @@
 export default function NotFound() {
   return (
-    <></>
+    <>
+    <h1 className="not-found-title">Ops!</h1>
+    
+    <h1 >
+      404 Página não encontrada!
+    </h1>
+
+    <img className="not-found-img-404" src="404.svg"/>
+    
+    <div>
+      <a href="/">Voltar para a página inicial</a>
+    </div>
+    </>
   )
 }
