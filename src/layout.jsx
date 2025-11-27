0368de1b9881dddd89c815214cb093ea63750185
Wanti <pedrowantuirpaixao@gmail.com>
2025-11-27 17:02:20 -0300

    página notFound estilizada. Footer estilizado

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import "./css/footer.css"
 
 export default function Layout() {
     return (
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,17 +1,22 @@
-export default function NotFound() {
-  return (
-    <>
-    <h1 className="not-found-title">Ops!</h1>
-    
-    <h1 >
-      404 Página não encontrada!
-    </h1>
 
-    <img className="not-found-img-404" src="404.svg"/>
-    
-    <div>
-      <a href="/">Voltar para a página inicial</a>
+
+export default function notFound() {
+  return (
+    <div className="not-found-container">
+      <h1 className="not-found-title">Ops!</h1>
+
+      <h1 className="not-found-text">
+        404 Página não encontrada!
+      </h1>
+
+
+      <img src="404.svg" className="not-found-img" />
+
+      <div className="not-found-button">
+        <a href="/">Voltar ao início</a>
+      </div>
+
     </div>
-    </>
   )
 }
+
